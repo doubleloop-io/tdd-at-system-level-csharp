5d15a4ec3e4bd3020aa9ec4ec45a839e0053fd9b
Massimo Iacolare <info@massimoiacolare.it>
2025-12-01 11:31:05 +0100

    Add TestReadEmployeeFile test

--- src/BirthdayGreetings.Tests/BirthdayGreetingsServiceTest.cs
+++ src/BirthdayGreetings.Tests/BirthdayGreetingsServiceTest.cs
@@ -5,12 +5,6 @@ namespace BirthdayGreetings.Tests;
 
 public class BirthdayGreetingsServiceTest
 {
-    [Fact]
-    public void ItWorks()
-    {
-        Assert.Equal("a", "a");
-    }
-
     [Fact(Skip = "Not there yet. too much stuff todo")]
     public void NoBirthday()
     {
@@ -48,5 +42,20 @@ public class BirthdayGreetingsServiceTest
         Assert.Equal("recipient@acme.com", received.ToAddresses[0].Address);
         Assert.Equal("Test subject", received.Subject);
         Assert.Equal("Test body", received.MessageParts[0].BodyData);
+    }
+
+    [Fact]
+    public void TestReadEmployeeFile()
+    {
+        File.WriteAllLines("employee-e2e.csv", [
+            "last_name, first_name, date_of_birth, email",
+            "Capone, Al, 1951-10-08, al.capone@acme.com",
+            "Escobar, Pablo, 1975-09-11, pablo.escobar@acme.com",
+            "Wick, John, 1987-09-11, john.wick@acme.com"
+        ]);
+
+        var allLines = File.ReadAllLines("employee-e2e.csv");
+        
+        Assert.Equal(4, allLines.Length);
     }
 }
