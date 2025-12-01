e36a5b34a63d505860d56529112e4b199e438df6
Massimo Iacolare <info@massimoiacolare.it>
2025-12-01 11:42:21 +0100

    Add NoBirthday test (fake it till you make it)

--- src/BirthdayGreetings.Tests/BirthdayGreetingsServiceTest.cs
+++ src/BirthdayGreetings.Tests/BirthdayGreetingsServiceTest.cs
@@ -5,23 +5,6 @@ namespace BirthdayGreetings.Tests;
 
 public class BirthdayGreetingsServiceTest
 {
-    [Fact(Skip = "Not there yet. too much stuff todo")]
-    public void NoBirthday()
-    {
-        // prepare employee file
-            // write file with some employees
-            // prepare file in advance
-        
-        // BirthdayGreetingsService.Run(2025-12-01)
-            // Read employee file
-            // for each employee
-            // if month and day matches
-                // create mail
-                // send mail
-                
-        // sent mail to smtp server is 0
-    }
-
     [Fact]
     public async Task TestSendMail()
     {
@@ -57,5 +40,34 @@ public class BirthdayGreetingsServiceTest
         var allLines = File.ReadAllLines("employee-e2e.csv");
         
         Assert.Equal(4, allLines.Length);
+    }
+
+    [Fact]
+    public void NoBirthday()
+    {
+        using var server = SimpleSmtpServer.Start(1025);
+        File.WriteAllLines("employee-e2e.csv", [
+            "last_name, first_name, date_of_birth, email",
+            "Capone, Al, 1951-10-08, al.capone@acme.com",
+            "Escobar, Pablo, 1975-09-11, pablo.escobar@acme.com",
+            "Wick, John, 1987-09-11, john.wick@acme.com"
+        ]);
+        var service = new BirthdayGreetingsService();
+        
+        service.Run(DateOnly.Parse("2025-12-01"));
+
+        Assert.Equal(0, server.ReceivedEmailCount);
+    }
+}
+
+public class BirthdayGreetingsService
+{
+    public void Run(DateOnly today)
+    {
+        // Read employee file
+        // for each employee
+        // if month and day matches
+        // create mail
+        // send mail
     }
 }
