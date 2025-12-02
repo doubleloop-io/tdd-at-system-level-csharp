a17efcc99dcdb532713c05a53cd9d29b7f1d23b2
Massimo Iacolare <info@massimoiacolare.it>
2025-12-02 10:58:43 +0100

    Extract SendMail

--- src/BirthdayGreetings/BirthdayGreetingsService.cs
+++ src/BirthdayGreetings/BirthdayGreetingsService.cs
@@ -38,22 +38,30 @@ public class BirthdayGreetingsService
 
             if (employee.IsBirthday(today))
             {
-                using var msg = new MailMessage(
-                    "greetings@acme.com",
-                    employee.Email,
-                    "Happy birthday!",
-                    $"Happy birthday, dear {employee.FirstName}!");
-
-                try
-                {
-                    using var smtp = new SmtpClient(smtpHost, smtpPort);
-                    await smtp.SendMailAsync(msg, cancellationToken);
-                }
-                catch (Exception ex)
-                {
-                    throw new Exception($"Smtp server unreachable at {smtpHost}:{smtpPort}", ex);
-                }
+                await SendMail(employee.FirstName, employee.Email, cancellationToken);
             }
+        }
+    }
+
+    private async Task SendMail(
+        string name,
+        string email, 
+        CancellationToken cancellationToken)
+    {
+        using var msg = new MailMessage(
+            "greetings@acme.com",
+            email,
+            "Happy birthday!",
+            $"Happy birthday, dear {name}!");
+
+        try
+        {
+            using var smtp = new SmtpClient(smtpHost, smtpPort);
+            await smtp.SendMailAsync(msg, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Smtp server unreachable at {smtpHost}:{smtpPort}", ex);
         }
     }
 }
